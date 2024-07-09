e7ee75a85d1c23771b1c95e6ccf7cf648be8ce2f
Pietro Liguori <pie.liguori@gmail.com>
2024-07-09 16:16:16 +0200

    Add files via upload

---
+++ boxplot_metrics.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+# Function to read data from file
+def read_values_from_file(file_path):
+    values = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            _, value = line.split(': ')
+            values.append(float(value.strip('%\n')))
+    return values
+
+# File path
+file_path = 'results/subset_output.txt'
+
+# Read data from file
+values = read_values_from_file(file_path)
+
+
+#Create the boxplot
+plt.figure(figsize=(10, 6))
+plt.boxplot(values, vert=True, patch_artist=True)
+
+# Titles and labels
+plt.title('Boxplot of Values')
+plt.xlabel('Values')
+
+# Show the plot
+plt.show()
